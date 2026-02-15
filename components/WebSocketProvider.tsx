40b5c9422e7cbd12e2aa87607cecff464152c2d7
Hugo Raskin <raskin.hugo@gmail.com>
2026-02-15 04:01:51 +0100

    for prod

--- components/WebSocketProvider.tsx
+++ components/WebSocketProvider.tsx
@@ -31,7 +31,7 @@ export function WebSocketProvider({
 	const [connected, setConnected] = useState(false);
 
 	useEffect(() => {
-		const socket = new SockJS(`${process.env.NEXT_PUBLIC_API_URL}/ws`);
+		const socket = new SockJS(`/ws`);
 
 		const client = new Client({
 			webSocketFactory: () => socket,
