886e5488ded493b4acc19bc11a36bcb9feb6f612
Hugo Raskin <raskin.hugo@gmail.com>
2026-02-15 04:03:47 +0100

    Fix string quotes in WebSocketProvider

--- components/WebSocketProvider.tsx
+++ components/WebSocketProvider.tsx
@@ -31,7 +31,7 @@ export function WebSocketProvider({
 	const [connected, setConnected] = useState(false);
 
 	useEffect(() => {
-		const socket = new SockJS(`/ws`);
+		const socket = new SockJS("/ws");
 
 		const client = new Client({
 			webSocketFactory: () => socket,
