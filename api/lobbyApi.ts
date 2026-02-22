63d06a1922e05680f1607be3079dd98b21d2d1af
Ska02 <114934118+Ska02@users.noreply.github.com>
2026-02-22 12:37:44 +0100

    remove unused team color variables and commented-out completeMission function

--- api/lobbyApi.ts
+++ api/lobbyApi.ts
@@ -17,19 +17,6 @@ export const assignTeam = async (
 	return await post(`/api/lobbies/${lobbyCode}/team`, { body: { clientId, teamLabel} });
 };
 
-export const completeMission = async (lobbyCode: string, clientId: string, missionNumber: string) => {
-    return await post(`/api/games/${lobbyCode}/complete`, {
-        body: {
-            clientId,
-            missionNumber,
-            resources: {
-                ENERGY: 0,
-                HUMAN: 0,
-            },
-        },
-    });
-};
-
 
 export const startLobby = async (lobbyCode: string, hostId: string) => {
     return await post(`/api/lobbies/${lobbyCode}/start`, {body: { hostId }});
