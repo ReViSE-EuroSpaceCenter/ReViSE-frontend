63ec03555a6357a84e5b7ba88607705ef68f755b
Ska02 <114934118+Ska02@users.noreply.github.com>
2026-02-22 12:33:32 +0100

    deletion of unused files

--- types/Checklist.ts
+++
@@ -1,7 +0,0 @@
-export const defaultChecklist = [
-    { id: 1, title: "Élément 1" },
-    { id: 2, title: "Élément 2" },
-    { id: 3, title: "Élément 3" },
-    //{ id: 4, title: "Élément 4" },
-];
-
