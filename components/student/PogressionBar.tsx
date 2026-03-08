4b92f0c55012ab0f05dc253ece957047c48acd04
Ska02 <114934118+Ska02@users.noreply.github.com>
2026-03-08 11:35:48 +0100

    refactor(mission-page): fix issues after sprint review

--- components/student/PogressionBar.tsx
+++ components/student/PogressionBar.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 type ProgressionBarProps = {
-    progression: number;
     completed: number;
     totalMission: number;
     color: string;
 }
 
-export function ProgressionBar({ progression, completed, totalMission, color }: Readonly<ProgressionBarProps>) {
+export function ProgressionBar({ completed, totalMission, color }: Readonly<ProgressionBarProps>) {
     return (
         <div className="flex flex-col gap-1 w-48 sm:w-64 lg:w-80">
             <div className="text-sm text-right">
@@ -16,7 +15,7 @@ export function ProgressionBar({ progression, completed, totalMission, color }: 
             <div className="w-full h-2 bg-white/20 rounded-full overflow-hidden">
                 <div
                     className="h-full rounded-full transition-all duration-500"
-                    style={{ width: `${progression}%`, backgroundColor: color }}
+                    style={{ width: `${(completed/totalMission)*100}%`, backgroundColor: color }}
                 />
             </div>
         </div>
