8749a136ee6fa1c4d9e633643d2063d227721007
erinfouarge <erin.fouarge@gmail.com>
2026-03-10 10:46:43 +0100

    Correction SonarQube error

--- utils/missionName.ts
+++ utils/missionName.ts
@@ -1,14 +1,17 @@
 import { Mission } from "@/types/Mission";
 
 export function missionNameTraduction(mission: Mission, teamName: string): string {
-    if (mission.bonus) {
-        if (teamName === "MECA") {
-            if (mission.id === 9) return "BONUS_1";
-            if (mission.id === 10) return "BONUS_2";
-        } else {
-            if (mission.id === 8) return "BONUS_1";
-            if (mission.id === 9) return "BONUS_2";
-        }
+    if (!mission.bonus) {
+        return `CLASSIC_${mission.id}`;
     }
+
+    if (teamName === "MECA") {
+        if (mission.id === 9) return "BONUS_1";
+        if (mission.id === 10) return "BONUS_2";
+    } else {
+        if (mission.id === 8) return "BONUS_1";
+        if (mission.id === 9) return "BONUS_2";
+    }
+
     return `CLASSIC_${mission.id}`;
 }
